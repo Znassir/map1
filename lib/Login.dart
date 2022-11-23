74fbc421e179c3146f5fa40b6c827e8dd7f74b56
abdullah-qutub <abdullah.qb@gmail.com>
2022-11-23 20:06:21 +0300

    added front to login page

--- lib/Login.dart
+++ lib/Login.dart
@@ -13,13 +13,27 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
           hexStringToColor("CB2B93"),
           hexStringToColor("9546C4"),
           hexStringToColor("5E61F4")
         ],begin: Alignment.topCenter ,end: Alignment.bottomCenter )),
+        child: SingleChildScrollView(child: Padding(padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).size.height *0.2, 20, 0),child:Column(
+          children: <Widget>[
+            logoWidget("Asset/img/logo.png")
+
+          ],) ,),),
       ),
     );
+    
+
+
+  }
+  Image logoWidget (String imageName ){
+    return Image.asset(imageName, fit : BoxFit.fitWidth, width: 240, height: 240,);
   }
 }
+
